372664d3cd7650b4761c6f88e0e7becca3db87fb
yyaroslavna <yaroslavna1008@gmail.com>
2026-07-26 18:41:37 +0200

    task 1(2)

---
+++ task1(2).py
@@ -0,0 +1,24 @@
+#Додатково для практики створила альтернативну версію функції,
+# яка запитує дату у користувача через input() 
+# та повторює введення до отримання коректного формату. 
+# Ця версія не є частиною домашнього завдання.
+
+from datetime import datetime
+
+def get_days_from_today():
+
+    now_date = datetime.today()
+
+    while True:
+        try:
+            user_date = input("Введіть дату (рррр-мм-дд): ")
+            specified_date = datetime.strptime(user_date, "%Y-%m-%d")
+            break
+        except ValueError:
+            print("Неправильний формат дати, спробуй ще раз")
+
+
+    difference_between_dates = now_date - specified_date
+    return difference_between_dates.days
+
+print("Різнийя між датами у днях:", get_days_from_today())
